4d1a9d8688663e216a2e2f101906662849b4a5f7
Ryo UMETSU <uryooo@gmail.com>
2023-09-11 01:52:43 +0900

    Updated site

--- config.rb
+++ config.rb
@@ -8,13 +8,13 @@ http_path = "/"
 http_images_path = "/images"
 http_generated_images_path = "/images"
 http_fonts_path = "/fonts"
-css_path = "/home/u-ryo/tmp/grain-theme-octopress-0.4.5/.cache/compass/stylesheets"
+css_path = "/home/u-ryo2/tmp/grain-theme-octopress-0.4.5/.cache/compass/stylesheets"
 
 # Local development paths
-sass_path = "/home/u-ryo/tmp/grain-theme-octopress-0.4.5/theme/sass"
-generated_images_path = "/home/u-ryo/tmp/grain-theme-octopress-0.4.5/.cache/compass/images"
-images_path = "/home/u-ryo/tmp/grain-theme-octopress-0.4.5/theme/images"
-fonts_path = "/home/u-ryo/tmp/grain-theme-octopress-0.4.5/theme/fonts"
+sass_path = "/home/u-ryo2/tmp/grain-theme-octopress-0.4.5/theme/sass"
+generated_images_path = "/home/u-ryo2/tmp/grain-theme-octopress-0.4.5/.cache/compass/images"
+images_path = "/home/u-ryo2/tmp/grain-theme-octopress-0.4.5/theme/images"
+fonts_path = "/home/u-ryo2/tmp/grain-theme-octopress-0.4.5/theme/fonts"
 
 line_comments = false
 output_style = :compressed
